56e7611e7f92ac83807c96659fe6a7e9ce8d4633
vital.kokhanau <harnet69@gmail.com>
2025-01-06 16:46:11 +0100

    Add API credentials

---
+++ composeApp/src/commonMain/kotlin/com/vital/kokhanau/currencyapp/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,8 @@
+package com.vital.kokhanau.currencyapp.data.remote.api
+
+class CurrencyApiService {
+    companion object {
+        const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+        const val API_KEY = "cur_live_sy6bKUxxaI5c20Zd0eIRUtzDJTAPNQvO48wzWtP7"
+    }
+}
